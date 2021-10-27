2d70dc70a12fc73c6c78d8b134be16e1ba3afe0d
Nofar Alon <nofaralon28@gmail.com>
2021-10-27 16:26:04 +0300

    add editor btn

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,16 +1,28 @@
 'use strict'
 
+
 function init() {
     console.log('hi');
     renderImgs()
+    gElCanvas = document.getElementById('my-canvas');
+    gCtx = gElCanvas.getContext('2d');
+
 }
 
 function renderImgs() {
     var imgs = getImgs()
 
     var strHtmls = imgs.map(function(img) {
-        return ` <div data-id="${img.id}" ><img src="${img.url}"></div>`
+        return ` <div onclick="onSelectedImg(${img.id})" data-id="${img.id}" ><img src="${img.url}"></div>`
     })
 
     document.querySelector('.img-container').innerHTML = strHtmls.join('');
+}
+
+function onSelectedImg(imgId) {
+    var elGallery = document.querySelector('.main-gallery')
+    elGallery.classList.add('hide')
+    var elCanvas = document.querySelector('.main-canvas')
+    elCanvas.classList.remove('hide')
+    drawImg2(imgId)
 }
--- js/memes-service.js
+++ js/memes-service.js
@@ -1,5 +1,8 @@
 'use strict'
 
+var gElCanvas;
+var gCtx;
+
 var gMeme = {
     selectedImgId: 5,
     selectedLineIdx: 0,
@@ -9,4 +12,13 @@ var gMeme = {
         align: 'left',
         color: 'red'
     }]
+}
+
+
+function drawImg2(imgId) {
+    var img = new Image();
+    img.src = `img/${imgId}.jpg`
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+    };
 }
