2515c2eff1b59b6153a4e8c3f0c1168b781350fa
Nofar Alon <nofaralon28@gmail.com>
2021-10-28 13:32:43 +0300

    add btn aditor

--- js/memes-service.js
+++ js/memes-service.js
@@ -9,10 +9,13 @@ var gMeme = {
     selectedImgId: 0,
     selectedLineIdx: 0,
     lines: [{
+        pos: { x: 50, y: 50 },
         txt: ' ',
-        size: 20,
-        align: 'left',
-        color: 'red'
+        size: 50,
+        align: 'center',
+        fillColor: 'white',
+        strokeColor: 'black',
+        font: 'impact'
     }]
 }
 
@@ -24,15 +27,19 @@ function updateGmeme(imgId) {
 }
 
 function drawText(x, y) {
-    var text = gMeme.lines[0].txt
+    // var text = gMeme.lines[0].txt
+    var line = gMeme.lines[0]
+    console.log(gMeme.lines[0]);
+    const { pos, txt, size, fillColor, strokeColor, font, align } = line
 
     gCtx.lineWidth = 2;
-    // gCtx.align = ''
-    gCtx.strokeStyle = 'black';
-    gCtx.fillStyle = 'white';
-    gCtx.font = '40px impact';
-    gCtx.fillText(text, x, y);
-    gCtx.strokeText(text, x, y);
+    gCtx.maxWidth = gElCanvas.width
+    gCtx.textAlign = align
+    gCtx.strokeStyle = strokeColor;
+    gCtx.fillStyle = fillColor;
+    gCtx.font = `${size}px ${font}`;
+    gCtx.fillText(txt, pos.x, pos.y, 500);
+    gCtx.strokeText(txt, pos.x, pos.y, 500);
 }
 
 function drawImg2(imgId) {
@@ -59,9 +66,10 @@ function downloadCanvas(elLink) {
 
 function renderTextTop() {
     var posTop = {
-        x: gElCanvas.width / 10,
-        y: gElCanvas.height / 10,
+        x: gElCanvas.width / 2,
+        y: gElCanvas.height / 4,
     }
+    gMeme.lines[0].pos = posTop
 
     drawText(posTop.x, posTop.y)
 }
@@ -70,12 +78,77 @@ function renderTextTop() {
 function onChangeTextTop(newText) {
     gMeme.lines[0].txt = newText
     drawImg2(gMeme.selectedImgId)
+    var elInput = document.querySelector('.text-meme')
+}
+
+
+function onFontIncrease() {
+    gMeme.lines[0].size += 2
+    drawImg2(gMeme.selectedImgId)
+
+}
+
+function onFontDcrease() {
+    gMeme.lines[0].size -= 2
+    drawImg2(gMeme.selectedImgId)
+}
+
+function onRemoveLine() {
+    gMeme.lines[0].txt = ' '
+    drawImg2(gMeme.selectedImgId)
+}
+
+function onSetfont(NewFont) {
+    gMeme.lines[0].font = NewFont
+    drawImg2(gMeme.selectedImgId)
+
+}
+
+function onAlignText(align) {
+    gMeme.lines[0].align = align
+    drawImg2(gMeme.selectedImgId)
+}
+
+function onSetStrokeText(strokeText) {
+    gMeme.lines[0].strokeColor = strokeText
+    drawImg2(gMeme.selectedImgId)
+}
+
+
+function onSetFillText(fillText) {
+    gMeme.lines[0].fillColor = fillText
+    drawImg2(gMeme.selectedImgId)
+}
+
+function onMove(movment) {
+    console.log(gMeme.lines[0].pos.x);
+    console.log(gMeme.lines[0].pos.y);
+
+    if (movment === 'up') {
+        gMeme.lines[0].pos.x += 2
+        gMeme.lines[0].pos.y += 2
+        drawImg2(gMeme.selectedImgId)
+    } else {
+        gMeme.lines[0].pos.x -= 2
+        gMeme.lines[0].pos.y -= 2
+        drawImg2(gMeme.selectedImgId)
+    }
+
+
 }
 
 
 
+function onMoveUp() {
+    console.log(gMeme.lines[0].pos.x);
+    console.log(gMeme.lines[0].pos.y);
+    gMeme.lines[0].pos.x += 100
+    gMeme.lines[0].pos.y += 100
+    console.log(gMeme.lines[0].pos.x);
+    console.log(gMeme.lines[0].pos.y);
+    drawImg2(gMeme.selectedImgId)
 
-
+}
 // var elInput = document.querySelector('.text-meme')
 // elInput.addEventListener('input', () => {
 //     console.log(gMeme.lines[0].txt);
